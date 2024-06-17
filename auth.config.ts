72fbdbee609242de1b93a16cb6e6356495a677f0
EzePerez <ezequielalejandroperez@hotmail.com>
2024-06-17 19:58:19 -0300

    modified:   auth.config.ts

--- auth.config.ts
+++ auth.config.ts
@@ -8,11 +8,10 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnAdmin = nextUrl.pathname.startsWith('/admin');
-      const isOnLogin = nextUrl.pathname.startsWith('/login');
       if (isOnAdmin) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn && isOnLogin) {
+      } else if (isLoggedIn && isOnAdmin) {
         return Response.redirect(new URL('/admin', nextUrl));
       }
       return true;
