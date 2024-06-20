a8f7bbb7ddb296d23630c8b55be96b6201c30c2c
EzePerez <ezequielalejandroperez@hotmail.com>
2024-06-20 19:25:11 -0300

    modified:   app/(home)/carrito/pago/page.tsx

--- app/(home)/carrito/pago/page.tsx
+++ app/(home)/carrito/pago/page.tsx
@@ -4,7 +4,7 @@ import EstadoPago from '@/app/ui/estadoPago'
 import {useSearchParams } from "next/navigation"
 import { Suspense } from 'react';
 
-export default async function Component(){
+export default function Component(){
 const paymentId = useSearchParams()?.get("payment_id") || "";    
 const status = useSearchParams()?.get("status") || ""; 
 
