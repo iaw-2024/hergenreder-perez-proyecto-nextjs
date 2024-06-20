099bb507c0a91d22eadcce61098aef1dc0db1cc1
EzePerez <ezequielalejandroperez@hotmail.com>
2024-06-20 19:32:04 -0300

    modified:   app/(home)/carrito/pago/page.tsx

--- app/(home)/carrito/pago/page.tsx
+++ app/(home)/carrito/pago/page.tsx
@@ -1,16 +1,21 @@
 'use client'
 
 import EstadoPago from '@/app/ui/estadoPago'
-import {useSearchParams } from "next/navigation"
+import { useSearchParams } from "next/navigation"
 import { Suspense } from 'react';
 
 export default function Component(){
-const paymentId = useSearchParams()?.get("payment_id") || "";    
-const status = useSearchParams()?.get("status") || ""; 
+
 
   return (
-    <Suspense>
-        <EstadoPago paymentId={paymentId} status={status}/>
+    <Suspense fallback={<div>Loading...</div>}>
+        <CarritoPagoContent/>
     </Suspense>
   );
 };
+
+function CarritoPagoContent() {
+  const paymentId = useSearchParams()?.get("payment_id") || "";    
+  const status = useSearchParams()?.get("status") || ""; 
+  return <EstadoPago paymentId={paymentId} status={status}/>
+}
