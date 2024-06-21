cbfb8a2d846893bf010b57fe0d6c7076ca1bd974
TomasHerg <tomahergenreder@gmail.com>
2024-06-21 19:59:56 -0300

    Update estadoPago.tsx

--- app/ui/estadoPago.tsx
+++ app/ui/estadoPago.tsx
@@ -1,55 +1,7 @@
 import Link from "next/link";
 import { Button, ButtonDeleteCart } from "./button";
-import { Payment } from "../lib/definitions";
-import { useState } from "react";
 
 export default async function EstadoPago({ paymentId, status }: { paymentId: string, status: string }) {
-
-    const [paymentToShow, insertPayment] = useState<Payment|null>(null);
-
-    const checkPayment = async (paymentId: any) => {
-
-        try{
-            const response = await fetch("api/payment_info/", {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    paymentId: paymentId
-                })
-            })
-            let infoPayment = await response.json(); 
-            let payment : Payment = {
-                id: infoPayment.id,
-                date_created: infoPayment.date_created,
-                date_approved: infoPayment.date_approved,
-                date_last_updated: infoPayment.date_last_updated,
-                money_release_date: infoPayment.money_release_date,
-                payment_method_id: infoPayment.payment_method_id,
-                payment_type_id: infoPayment.payment_type_id,
-                status: infoPayment.status,
-                status_detail: infoPayment.status_detail,
-                currency_id: infoPayment.currency_id,
-                description: infoPayment.description,
-                collector_id: infoPayment.collector_id,
-                payer: {
-                    id: infoPayment.payer.id,
-                    email: infoPayment.payer.email,
-                    identification: {
-                        type: infoPayment.payer.identification.type,
-                        number: infoPayment.payer.identification.number
-                    }
-                }
-            }
-
-            console.log(payment);
-            insertPayment(payment);
-        }
-        catch(error){
-
-        }
-    }
 
     return (
         <div className="text-white bg-gray-950 shadow-sm rounded-lg max-w-3xl mx-auto p-6">
@@ -58,9 +10,8 @@ export default async function EstadoPago({ paymentId, status }: { paymentId: str
                 <div className="text-gray-400 justify-center">
                     <h1 className="text-l font-bold">Detalle de pago:</h1>
                     <div className="px-6">
-                        {paymentToShow != null && <p>ID de Pago: {paymentToShow?.id}</p>}
-                        {paymentToShow != null && <p>Estado: {paymentToShow?.status}</p>}    
-                        {paymentToShow != null && <p>email: {paymentToShow?.payer.email}</p>}    
+                        <p>ID de Pago: {paymentId}</p>
+                        <p>Estado: {status}</p>
                     </div>
                 </div>    
             </div>
